8da30b554df5eee89b8945162fd5f4973bf0f09e
Victor Rivero <victorrivero2415@gmail.com>
2021-11-24 14:20:20 -0500

    Allow shipping facade to be initialized statically and added a few more helper methods

--- src/Shipping.php
+++ src/Shipping.php
@@ -9,24 +9,21 @@ use Illuminate\Support\Collection;
 
 class Shipping
 {
-    /**
-     * Collection of shipping service providers
-     *
-     * @var \Illuminate\Support\Collection
-     */
     protected Collection $providers;
+
+    protected ?array $parameters = null;
 
     /**
      * Create a new instance of the specified provider.
      *
      * @param string $name
      * @param array $config
-     * @param array $parameters
+     * @param array|null $parameters
      * @return ShippingProvider
      *
      * @throws \Throwable
      */
-    public static function provider(string $name, array $config = [], array $parameters = []): ShippingProvider
+    public static function provider(string $name, array $config, ?array $parameters = null): ShippingProvider
     {
         $providers = config('shipping.providers');
 
@@ -36,17 +33,115 @@ class Shipping
     }
 
     /**
-     * Create new instance of the class
-     *
-     * @param array $configs
-     * @param array $parameters
+     * Initialize class statically with the provided shipping providers
      *
      * @throws \Throwable
      */
-    public function __construct(array $configs, array $parameters)
+    public static function providers(array $configs, ?array $parameters = null): static
     {
+        return new static($configs, $parameters);
+    }
+
+    /**
+     * Create new instance of the class
+     *
+     * @param array $configs
+     * @param array|null $parameters
+     *
+     * @throws \Throwable
+     */
+    final public function __construct(array $configs, ?array $parameters = null)
+    {
+        $this->parameters = $parameters;
+
         $this->providers = collect($configs)
-            ->mapWithKeys(fn ($config, $provider) => [$provider => self::provider($provider, $config, $parameters)]);
+            ->mapWithKeys(fn ($config, $provider) => [
+                $provider => self::provider($provider, $config, $this->parameters),
+            ]);
+    }
+
+    /**
+     * Set parameters for all providers.
+     *
+     * @param array $parameters
+     * @return $this
+     */
+    public function setParameters(array $parameters): static
+    {
+        $this->parameters = $parameters;
+
+        $this->providers->each(function (ShippingProvider $provider) {
+            if (isset($this->parameters['consignor'])) {
+                $provider->setConsignor($this->parameters['consignor']);
+            }
+
+            if (isset($this->parameters['consignee'])) {
+                $provider->setConsignee($this->parameters['consignee']);
+            }
+
+            if (isset($this->parameters['package'])) {
+                $provider->setPackage($this->parameters['package']);
+            }
+        });
+
+        return $this;
+    }
+
+    /**
+     * Add new provider to the already existing providers.
+     *
+     * @param string $name
+     * @param array $config
+     * @return $this
+     * @throws \Throwable
+     */
+    public function addProvider(string $name, array $config): static
+    {
+        $this->providers->put($name, self::provider($name, $config, $this->parameters));
+
+        return $this;
+    }
+
+    /**
+     * Get provider from the list of initialized providers
+     *
+     * @param string $name
+     * @return $this
+     */
+    public function getProvider(string $name): static
+    {
+        return $this->providers->get($name);
+    }
+
+    /**
+     * Update provider configs and parameters.
+     *
+     * @param string $name
+     * @param array $config
+     * @return $this
+     * @throws \Throwable
+     */
+    public function updateProvider(string $name, array $config): static
+    {
+        $provider = $this->providers->get($name);
+
+        $provider->setConfig($config);
+        $provider->setConfig($this->parameters);
+
+        return $this;
+    }
+
+    /**
+     * Remove provider from the list of initialized providers.
+     *
+     * @param string $name
+     * @return $this
+     */
+    public function removeProvider(string $name): static
+    {
+        $this->providers->forget($name);
+
+        return $this;
     }
 
     /**
@@ -61,8 +156,7 @@ class Shipping
             return $this->providers->get($provider)->getCarriers();
         }
 
-        return $this->providers
-            ->values()
+        return $this->providers->values()
             ->map(fn (ShippingProvider $provider) => $provider->getCarriers())
             ->flatten(1);
     }
@@ -79,8 +173,7 @@ class Shipping
             return $this->providers->get($provider)->getServices();
         }
 
-        return $this->providers
-            ->values()
+        return $this->providers->values()
             ->map(fn (ShippingProvider $provider) => $provider->getServices())
             ->flatten(1);
     }
@@ -98,8 +191,7 @@ class Shipping
             return $this->providers->get($provider)->getRates($parameters);
         }
 
-        return $this->providers
-            ->values()
+        return $this->providers->values()
             ->map(fn (ShippingProvider $provider) => $provider->getRates($parameters))
             ->flatten(1);
     }
